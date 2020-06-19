fd426b4982a7b44be76067cebbdbcda9ed93b0af
Amit998 <damitlucky998@gmail.com>
2020-06-19 12:15:00 +0530

    Quiz

---
+++ app.js
@@ -0,0 +1,48 @@
+const correctAnswers =['C','B','B','D'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+
+    let score= 0;
+    const userAnswers=[form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    // Check Answers
+
+    userAnswers.forEach((answer,index) => {
+        if (answer === correctAnswers[index]) {
+            score+=25;
+
+            
+            
+        }
+    });
+    //console.log(score);
+
+    // document.getElementById("demo").innerHTML=score;
+    
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+    let output= 0;
+            const timer = setInterval(()=>{
+                result.querySelector('span').textContent=`${output}%`;
+                if(output===score){
+                    clearInterval(timer);
+                }else{
+                    output++;
+                }
+            },20);
+    
+    
+    
+});
+//window object(global scope)
+
+// let i=0;
+// const timer=setInterval(()=>{
+//     //console.log('hello');
+//     i++;
+//     if(i===5){
+//         clearInterval(timer);
+//     }
+// },1000);
